31af9fd537fbfef7750d6ddd6b2681594235472f
jamal <jamal@marketport.io>
2024-02-14 16:12:35 +0000

    commit again

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main(){
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Jamalul Insan hellow kali Mas Ek !")	
+		return c.SendString("Hello Mas Jamal")	
 	})
 	log.Fatal(app.Listen(":3000"))
 }
